fba552142a2366022b63e6631833bf67e36e016c
Jimmy-Gan <zjgump@gmail.com>
2017-01-19 01:23:42 +0800

    working on Wherefor-art-thou.js

--- sets/Wherefore-art-thou.js
+++ sets/Wherefore-art-thou.js
@@ -3,16 +3,26 @@ function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
   var len = collection.length;
-  
+  var sourcekeylen = Object.keys(source).length;
+
+// get [1]soure keys and [2]collection[i] keys in an array
+// compare, pass only if all [1] exist in [2] and [1]values == [2]values;
+// known issue : count does not work well.
+
   for (var i = 0; i < len; i++) {
-    if (source in collection) {
-    console.log("yes")
-    } 
+    for (var j = 0; j < sourcekeylen; j++){
+      var keytocheck = Object.keys(source)[j];
+      var count = 0;
+      if (source[keytocheck] !== collection[i][keytocheck]) {
+        console.log("no");
+	break;
+      }else {count++}
+    }
+    if (count === sourcekeylen) {arr.push(collection[i])};
   }
-  console.log(source) 
+  console.log(arr); 
   // Only change code above this line
   return arr;
 }
 
-whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
-
+whatIsInAName([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 })
